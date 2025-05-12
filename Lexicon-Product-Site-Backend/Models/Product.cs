68c201cd530585e299f1948c229058b0fa4a0f91
Zedd Fox <25223537+ZeddFox@users.noreply.github.com>
2025-05-12 11:58:58 +0200

    Add Product model

---
+++ Lexicon-Product-Site-Backend/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lexicon_Product_Site_Backend.Models
+{
+    public class Product
+    {
+        [Key]
+        public int? ProductID { get; set; }
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+        public bool? Enabled { get; set; }
+    }
+}
